84decbf328d669b76aa85ef14cfb216f550bc19a
shravkv <sravan.k.vodnala@gmail.com>
2022-08-18 21:27:56 +0530

    Added: Add Submit and Reset Button at the Bottom of the Form

--- js/AddFormValidation.js
+++ js/AddFormValidation.js
@@ -40,3 +40,60 @@ function validateInputs() {
         }
     });
 }
+
+//Declaring save method
+const save = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    let addressBook = createAddressBook();
+    addAndUpdateLocalStorage(addressBook); //Data store in local storage
+    alert("Data is being added successfully " + addressBook._name);
+    window.location.replace(Site_Properties.home);
+}
+
+const createAddressBook = () => {
+    let addressBook = new AddressBook();
+    addressBook.name = getInputValueId("#name");
+    addressBook.phone = getInputValueId("#phone");
+    addressBook.address = getInputValueId("#address");
+    addressBook.city = getSelectedValue('[name=city]').pop();
+    addressBook.state = getSelectedValue('[name=state]').pop();
+    addressBook.zipcode = getInputValueId("#zipcode");
+    return addressBook;
+}
+
+const getInputValueId = (id) => {
+    return document.querySelector(id).value;
+}
+
+const setTextValue = (id, message) => {
+    const textError = document.querySelector(id);
+    textError.textContent = message;
+}
+
+const getSelectedValue = (propertyValue) => {
+    let allItem = document.querySelectorAll(propertyValue);
+    let setItem = [];
+    allItem.forEach(item => {
+        if (item.checked == true) {
+            setItem.push(item.value);
+        }
+    })
+    return setItem;
+}
+
+//Data storing in local storage
+const addAndUpdateLocalStorage = (persons) => {
+    let personList = JSON.parse(localStorage.getItem("AddressBookList"));
+    if (personList != undefined) {
+        personList.push(persons);
+    } else {
+        personList = [persons];
+    }
+    localStorage.setItem('AddressBookList', JSON.stringify(personList));
+}
+
+//Reset method
+const reset = () => {
+    alert("Reset button");
+}
